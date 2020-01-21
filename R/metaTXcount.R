9b099cb065f450d5b9cbc6601c8bf8f7c373c44f
yue <15905529958@163.com>
2020-01-21 20:10:20 +0800

    updated

--- R/metaTXcount.R
+++ R/metaTXcount.R
@@ -1,9 +1,12 @@
 metaTXcount <-
-function (features, txdb, num_bin = 10, includeNeighborDNA = FALSE) 
-{
-    remap_results <- remapCoord(methyl, txdb, num_bin, includeNeighborDNA)
-    align_mtr <- remap_results[[1]]
-    num_bin_sum <- ncol(align_mtr)
-    feature_counts <- colSums(align_mtr)
+function(features,
+                        txdb,
+                        num_bin = 10,
+                        includeNeighborDNA = FALSE
+){
+    remap_results      <- remapCoord(methyl, txdb, num_bin, includeNeighborDNA)
+    align_mtr          <- remap_results[[1]]
+    num_bin_sum        <- ncol(align_mtr)
+    feature_counts     <- colSums(align_mtr)
     return(feature_counts)
 }
