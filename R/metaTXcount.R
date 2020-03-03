2d9213ef6ad93adb62080d0bc8e6c432f2a0ff61
yue <15905529958@163.com>
2020-03-03 17:14:43 +0800

    updated

--- R/metaTXcount.R
+++ R/metaTXcount.R
@@ -1,12 +1,8 @@
 metaTXcount <-
-function(features,
-                        txdb,
-                        num_bin = 10,
-                        includeNeighborDNA = FALSE
-){
-    remap_results      <- remapCoord(methyl, txdb, num_bin, includeNeighborDNA)
-    align_mtr          <- remap_results[[1]]
-    num_bin_sum        <- ncol(align_mtr)
-    feature_counts     <- colSums(align_mtr)
-    return(feature_counts)
-}
+function(remap_results
+    ){
+        align_mtr          <- remap_results[[1]]
+        num_bin_sum        <- ncol(align_mtr)
+        feature_counts     <- colSums(align_mtr)
+        return(feature_counts)
+    }
