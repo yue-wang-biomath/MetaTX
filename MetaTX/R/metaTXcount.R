15e11fd97fea2cbf914c388889666692f857a47b
yue-wang-biomath <59797375+yue-wang-biomath@users.noreply.github.com>
2020-06-20 02:19:55 +0800

    Delete metaTXcount.R

--- MetaTX/R/metaTXcount.R
+++
@@ -1,8 +0,0 @@
-metaTXcount <-
-function(remap_results
-    ){
-        align_mtr          <- remap_results[[1]]
-        num_bin_sum        <- ncol(align_mtr)
-        feature_counts     <- colSums(align_mtr)
-        return(feature_counts)
-    }
